90eda82f1789b9c3d25916536d78d23dbea359bd
Can202 <mgoopazo@gmail.com>
2021-07-14 12:09:15 -0400

    mpd_can

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
 
-mpd={}
+mpd_can={}
 
 
-mpd.pause_between_songs=tonumber(minetest.settings:get("mpd_pause_between_songs")) or 30
+mpd_can.pause_between_songs=tonumber(minetest.settings:get("mpd_can_pause_between_songs")) or 30
 
 --end config
 
-mpd.modpath=minetest.get_modpath("mpd")
-if not mpd.modpath then
-	error("mpd mod folder has to be named 'mpd'!")
+mpd_can.modpath=minetest.get_modpath("mpd_can")
+if not mpd_can.modpath then
+	error("mpd_can mod folder has to be named 'mpd_can'!")
 end
 --{name, length, gain~1}
-mpd.songs = {}
-local sfile, sfileerr=io.open(mpd.modpath..DIR_DELIM.."songs.txt")
+mpd_can.songs = {}
+local sfile, sfileerr=io.open(mpd_can.modpath..DIR_DELIM.."songs.txt")
 if not sfile then error("Error opening songs.txt: "..sfileerr) end
 for linent in sfile:lines() do
 	-- trim leading and trailing spaces away
@@ -22,154 +22,154 @@ for linent in sfile:lines() do
 		local timeMins, timeSecs, gain = tonumber(timeMinsStr), tonumber(timeSecsStr), tonumber(gainStr)
 		if title=="" then title = name end
 		if name and timeMins and timeSecs and gain then
-			mpd.songs[#mpd.songs+1]={name=name, length=timeMins*60+timeSecs, lengthhr=timeMinsStr..":"..timeSecsStr, gain=gain, title=title}
+			mpd_can.songs[#mpd_can.songs+1]={name=name, length=timeMins*60+timeSecs, lengthhr=timeMinsStr..":"..timeSecsStr, gain=gain, title=title}
 		else
-			minetest.log("warning", "[mpd] Misformatted song entry in songs.txt: "..line)
+			minetest.log("warning", "[mpd_can] Misformatted song entry in songs.txt: "..line)
 		end
 	end
 end
 sfile:close()
 
-if #mpd.songs==0 then
-	print("[mpd]no songs registered, not doing anything")
+if #mpd_can.songs==0 then
+	print("[mpd_can]no songs registered, not doing anything")
 	return
 end
 
-mpd.storage = minetest.get_mod_storage()
+mpd_can.storage = minetest.get_mod_storage()
 
-mpd.handles={}
+mpd_can.handles={}
 
-mpd.playing=false
-mpd.id_playing=nil
-mpd.song_time_left=nil
-mpd.time_next=10 --sekunden
-mpd.id_last_played=nil
+mpd_can.playing=false
+mpd_can.id_playing=nil
+mpd_can.song_time_left=nil
+mpd_can.time_next=10 --sekunden
+mpd_can.id_last_played=nil
 
 minetest.register_globalstep(function(dtime)
-	if mpd.playing then
-		if mpd.song_time_left<=0 then
-			mpd.stop_song()
-			mpd.time_next=mpd.pause_between_songs
+	if mpd_can.playing then
+		if mpd_can.song_time_left<=0 then
+			mpd_can.stop_song()
+			mpd_can.time_next=mpd_can.pause_between_songs
 		else
-			mpd.song_time_left=mpd.song_time_left-dtime
+			mpd_can.song_time_left=mpd_can.song_time_left-dtime
 		end
-	elseif mpd.time_next then
-		if mpd.time_next<=0 then
-			mpd.next_song()
+	elseif mpd_can.time_next then
+		if mpd_can.time_next<=0 then
+			mpd_can.next_song()
 		else
-			mpd.time_next=mpd.time_next-dtime
+			mpd_can.time_next=mpd_can.time_next-dtime
 		end
 	end
 end)
-mpd.play_song=function(id)
-	if mpd.playing then
-		mpd.stop_song()
+mpd_can.play_song=function(id)
+	if mpd_can.playing then
+		mpd_can.stop_song()
 	end
-	local song=mpd.songs[id]
+	local song=mpd_can.songs[id]
 	if not song then return end
 	for _,player in ipairs(minetest.get_connected_players()) do
 		local pname=player:get_player_name()
-		local pvolume=tonumber(mpd.storage:get_string("vol_"..pname))
+		local pvolume=tonumber(mpd_can.storage:get_string("vol_"..pname))
 		if not pvolume then pvolume=1 end
 		if pvolume>0 then
 			local handle = minetest.sound_play(song.name, {to_player=pname, gain=song.gain*pvolume})
 			if handle then
-				mpd.handles[pname]=handle
+				mpd_can.handles[pname]=handle
 			end
 		end
 	end
-	mpd.playing=id
+	mpd_can.playing=id
 	--adding 2 seconds as security
-	mpd.song_time_left = song.length + 2
+	mpd_can.song_time_left = song.length + 2
 end
-mpd.stop_song=function()
-	for pname, handle in pairs(mpd.handles) do
+mpd_can.stop_song=function()
+	for pname, handle in pairs(mpd_can.handles) do
 		minetest.sound_stop(handle)
 	end
-	mpd.id_last_played=mpd.playing
-	mpd.playing=nil
-	mpd.handles={}
-	mpd.time_next=nil
+	mpd_can.id_last_played=mpd_can.playing
+	mpd_can.playing=nil
+	mpd_can.handles={}
+	mpd_can.time_next=nil
 end
 
-mpd.next_song=function()
+mpd_can.next_song=function()
 	local next
 	repeat
-		next=math.random(1,#mpd.songs)
-	until #mpd.songs==1 or next~=mpd.id_last_played
-	mpd.play_song(next)
+		next=math.random(1,#mpd_can.songs)
+	until #mpd_can.songs==1 or next~=mpd_can.id_last_played
+	mpd_can.play_song(next)
 end
 
-mpd.song_human_readable=function(id)
+mpd_can.song_human_readable=function(id)
 	if not tonumber(id) then return "<error>" end
-	local song=mpd.songs[id]
+	local song=mpd_can.songs[id]
 	if not song then return "<error>" end
 	return id..": "..song.title.." ["..song.lengthhr.."]"
 end
 
-minetest.register_privilege("mpd", "may control the music player daemon (mpd) mod")
+minetest.register_privilege("mpd_can", "may control the music player daemon (mpd_can) mod")
 
-minetest.register_chatcommand("mpd_stop", {
+minetest.register_chatcommand("mpd_can_stop", {
 	params = "",
 	description = "Stop the song currently playing",
-	privs = {mpd=true},
+	privs = {mpd_can=true},
 	func = function(name, param)
-		mpd.stop_song()
+		mpd_can.stop_song()
 	end,		
 })
-minetest.register_chatcommand("mpd_list", {
+minetest.register_chatcommand("mpd_can_list", {
 	params = "",
 	description = "List all available songs and their IDs",
-	privs = {mpd=true},
+	privs = {mpd_can=true},
 	func = function(name, param)
-		for k,v in ipairs(mpd.songs) do
-			minetest.chat_send_player(name, mpd.song_human_readable(k))
+		for k,v in ipairs(mpd_can.songs) do
+			minetest.chat_send_player(name, mpd_can.song_human_readable(k))
 		end
 	end,		
 })
-minetest.register_chatcommand("mpd_play", {
+minetest.register_chatcommand("mpd_can_play", {
 	params = "<id>",
-	description = "Play the songs with the given ID (see ids with /mpd_list)",
-	privs = {mpd=true},
+	description = "Play the songs with the given ID (see ids with /mpd_can_list)",
+	privs = {mpd_can=true},
 	func = function(name, param)
 		if param=="" then
-			mpd.next_song()
-			return true,"Playing: "..mpd.song_human_readable(mpd.playing)
+			mpd_can.next_song()
+			return true,"Playing: "..mpd_can.song_human_readable(mpd_can.playing)
 		end
 		id=tonumber(param)
-		if id and id>0 and id<=#mpd.songs then
-			mpd.play_song(id)
-			return true,"Playing: "..mpd.song_human_readable(id)
+		if id and id>0 and id<=#mpd_can.songs then
+			mpd_can.play_song(id)
+			return true,"Playing: "..mpd_can.song_human_readable(id)
 		end
 		return false, "Invalid song ID!"
 	end,		
 })
-minetest.register_chatcommand("mpd_what", {
+minetest.register_chatcommand("mpd_can_what", {
 	params = "",
 	description = "Display the currently played song.",
-	privs = {mpd=true},
+	privs = {mpd_can=true},
 	func = function(name, param)
-		if not mpd.playing then
-			if mpd.time_next and mpd.time_next~=0 then
-				return true,"Nothing playing, "..math.floor(mpd.time_next or 0).." sec. left until next song."
+		if not mpd_can.playing then
+			if mpd_can.time_next and mpd_can.time_next~=0 then
+				return true,"Nothing playing, "..math.floor(mpd_can.time_next or 0).." sec. left until next song."
 			else
 				return true,"Nothing playing."
 			end
 		end
-		return true,"Playing: "..mpd.song_human_readable(mpd.playing).."\nTime Left: "..math.floor(mpd.song_time_left or 0).." sec."
+		return true,"Playing: "..mpd_can.song_human_readable(mpd_can.playing).."\nTime Left: "..math.floor(mpd_can.song_time_left or 0).." sec."
 	end,		
 })
-minetest.register_chatcommand("mpd_next", {
+minetest.register_chatcommand("mpd_can_next", {
 	params = "[seconds]",
 	description = "Start the next song, either immediately (no parameters) or after n seconds.",
-	privs = {mpd=true},
+	privs = {mpd_can=true},
 	func = function(name, param)
-		mpd.stop_song()
+		mpd_can.stop_song()
 		if param and tonumber(param) then
-			mpd.time_next=tonumber(param)
+			mpd_can.time_next=tonumber(param)
 			return true,"Next song in "..param.." seconds!"
 		else
-			mpd.next_song()
+			mpd_can.next_song()
 			return true,"Next song started!"
 		end
 	end,		
@@ -180,13 +180,13 @@ minetest.register_chatcommand("mvolume", {
 	privs = {},
 	func = function(pname, param)
 		if not param or param=="" then
-			local pvolume=tonumber(mpd.storage:get_string("vol_"..pname))
+			local pvolume=tonumber(mpd_can.storage:get_string("vol_"..pname))
 			if not pvolume then pvolume=0.5 end
 			if pvolume>0 then
 				return true, "Your music volume is set to "..pvolume.."."
 			else
-				if mpd.handles[pname] then
-					minetest.sound_stop(mpd.handles[pname])
+				if mpd_can.handles[pname] then
+					minetest.sound_stop(mpd_can.handles[pname])
 				end
 				return true, "Background music is disabled for you. Use '/mvolume 1' to enable it again."
 			end
@@ -197,12 +197,12 @@ minetest.register_chatcommand("mvolume", {
 		end
 		pvolume = math.min(pvolume, 1)
 		pvolume = math.max(pvolume, 0)
-		mpd.storage:set_string("vol_"..pname, pvolume)
+		mpd_can.storage:set_string("vol_"..pname, pvolume)
 		if pvolume>0 then
 			return true, "Music volume set to "..pvolume..". Change will take effect when the next song starts."
 		else
-			if mpd.handles[pname] then
-				minetest.sound_stop(mpd.handles[pname])
+			if mpd_can.handles[pname] then
+				minetest.sound_stop(mpd_can.handles[pname])
 			end
 			return true, "Disabled background music for you. Use /mvolume to enable it again."
 		end
@@ -210,5 +210,5 @@ minetest.register_chatcommand("mvolume", {
 })
 
 if vote then
-	dofile(mpd.modpath..DIR_DELIM.."vote.lua")
+	dofile(mpd_can.modpath..DIR_DELIM.."vote.lua")
 end
